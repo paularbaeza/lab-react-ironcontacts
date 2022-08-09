8e361e1e451924f667d1e8e48247974a01c41556
Paula Rodríguez <paularb923@hotmail.com>
2022-08-09 15:38:36 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import {useState} from "react"
 function App() {
 
   const [contactsToDisplay, setContactsToDisplay] =useState(allContacts.slice(0,5))
-  
-  
   return (
     <div className="App">
-      <h3>Contacts</h3>
+      <h1>IronContacts</h1>
       {contactsToDisplay.map((eachContact)=> {
         return (
           <table>
@@ -18,11 +16,15 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
           <tr>
            <td><img src={eachContact.pictureUrl} alt="contact" width="100" /></td>
            <td><p>{eachContact.name}</p></td>
            <td> <p>{eachContact.popularity.toFixed(2)}</p></td>
+           <td>{eachContact.wonOscar  ? "🏆" : ""}</td>
+           <td>{eachContact.wonEmmy ? "🏆" : ""}</td>
           </tr>
           </table>
         )
